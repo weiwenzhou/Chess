c20bca7c3005e58833e499f8645a4cedaeffb071
jsun5 <jsun5@stuy.edu>
2018-01-04 17:20:19 -0500

    wrote Piece.java skeleton

---
+++ Piece.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Piece extends JLabel{
+    private Coords position;
+    private int color;
+    
+    public Piece(int x, int y, int col){
+    
+    }
+    
+    public Piece(Coords coor, int col){
+        
+    }
+    
+    public Arraylist<ArrayList<Coords>> getValidMoves{
+        
+    }
+    
+    public ArrayList<Coords> getMoves(int x, int y, int xdirection, int ydirection){
+    
+    }
+    
+    public Coords getPosition(){
+        return position;
+    }
+    
+    public void setPosition(Coords coor){
+        position = coor;
+    }
+    
+    public int getColor(){
+        return color;
+    }
+    
+    public boolean inBetween(int num, int low, int high){
+        
+    }
+    
+    public String toString(){
+        
+    }
+}
