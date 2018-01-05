9ad5fce74ac83f5fab078a65c944972790d91e8b
jsun5 <jsun5@stuy.edu>
2018-01-05 14:48:38 -0500

    update

--- Coords.java
+++ Coords.java
@@ -1,4 +1,4 @@
-public class Coords{//
+public class Coords{
     private int x,y;
 
     public Coords(int xcor, int ycor){
@@ -25,4 +25,4 @@ public class Coords{//
     public boolean equals(Coords other){
 	return true;
     }
-}//
+}
