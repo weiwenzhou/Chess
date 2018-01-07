ae5db70b831fb86d917cbaa6e17baa15f4222586
wzhou2 <wzhou2@stuy.edu>
2018-01-07 17:31:53 -0500

    Added fields and placeholders for constructor and methods

---
+++ Pawn.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Pawn {
+    private boolean firstMove;
+    private ImageIcon[] icons;
+    
+    Pawn(int, int, int) {
+        
+    }
+    
+    Pawn (Coords, int) {
+        
+    }
+    
+    public ArrayList<ArrayList<Coords>> getValidMoves() {
+        
+    }
+    
+    public void notFirst() {
+        
+    }
+    
+    private int getDirection() {
+        
+    }
+    
+    public ArrayList<Coords> getKillMoves() {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    private void setIcon() {
+        
+    }
+
+}
