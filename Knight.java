2d7477d1a79eacccf60309e45c6c46be1fc10a90
wzhou2 <wzhou2@stuy.edu>
2018-01-07 17:40:04 -0500

    Added fields and placeholders for constructor and methods

---
+++ Knight.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Knight {
+    private boolean firstMove;
+    private ImageIcon[] icons;
+    
+    public Knight(int, int, int) {
+        
+    }
+    
+    public Knight(Coords, int) {
+        
+    }
+    
+    public ArrayList<ArrayList<Coords>> getValidMoves() {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    private void setIcon() {
+        
+    }
+
+}
