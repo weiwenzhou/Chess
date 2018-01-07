74d5719a623acbb84dffd7f8a37d242417f2a7be
wzhou2 <wzhou2@stuy.edu>
2018-01-07 17:35:26 -0500

    Added fields and placeholders for constructor and methods

---
+++ Rook.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Rook {
+    private boolean firstMove;
+    private ImageIcon[] icons;
+    
+    public Rook(int, int, int) {
+        
+    }
+    
+    public Rook(Coords, int) {
+        
+    }
+    
+    public ArrayList<ArrayList<Coords>> getValidMoves() {
+        
+    }
+    
+    public void notFirst() {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    private void setIcon() {
+        
+    }
+
+}
