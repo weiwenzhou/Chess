8545d569c8b3a14c4f125d05e112459258fd8ea7
wzhou2 <wzhou2@stuy.edu>
2018-01-09 01:40:20 -0500

    wrote getValidMoves and tested

--- Knight.java
+++ Knight.java
@@ -19,6 +19,44 @@ public class Knight extends Piece{
     
     public ArrayList<ArrayList<Coords>> getValidMoves() {
         ArrayList<ArrayList<Coords>> validSet = new ArrayList<ArrayList<Coords>>();
+        ArrayList<Coords> validSetMoves = new ArrayList<Coords>();
+        int xcor = getPosition().getX();
+        int ycor = getPosition().getY();
+        
+         if (inBetween(xcor-2) && inBetween(ycor-1)) {
+                validSetMoves.add(new Coords(xcor - 2,ycor - 1));
+        }
+        if (inBetween(xcor-2) && inBetween(ycor+1)) {
+                validSetMoves.add(new Coords(xcor - 2,ycor + 1));
+        }
+        if (inBetween(xcor-1) && inBetween(ycor-2)) {
+                validSetMoves.add(new Coords(xcor - 1,ycor - 2));
+        }
+        if (inBetween(xcor-1) && inBetween(ycor+2)) {
+                validSetMoves.add(new Coords(xcor - 1,ycor + 2));
+        }
+        if (inBetween(xcor+1) && inBetween(ycor-2)) {
+                validSetMoves.add(new Coords(xcor + 1,ycor - 2));
+        }
+        if (inBetween(xcor+1) && inBetween(ycor+2)) {
+                validSetMoves.add(new Coords(xcor + 1,ycor + 2));
+        }
+        if (inBetween(xcor+2) && inBetween(ycor-1)) {
+                validSetMoves.add(new Coords(xcor + 2,ycor - 1));
+        }
+        if (inBetween(xcor+2) && inBetween(ycor+1)) {
+                validSetMoves.add(new Coords(xcor + 2,ycor + 1));
+        }
+        
+        // verification of positions
+        for (int x = 0; x < 8; x++) {
+            Coords currentPosition = validSetMoves.get(0);
+            validSetMoves.remove(0);
+            if (Board.getPiece(currentPosition).getColor() != getColor()) {
+                validSetMoves.add(currentPosition);
+            }
+        }
+        validSet.add(validSetMoves);
         return validSet;
     }
     
