40f400aee218ed8278ac453c6b40022c18239536
wzhou2 <wzhou2@stuy.edu>
2018-01-08 20:36:09 -0500

    Redirect one constructor to the other. Set icons in constructor

--- Pawn.java
+++ Pawn.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 public class Pawn {
     private boolean firstMove;
-    private ImageIcon[] icons;
     
     Pawn(int x, int y, int c) {
-        super(x, y, c);
-        firstMove = true;
+        Pawn(new Coords(x, y), c);
     }
     
     Pawn (Coords coor, int c) {
         super(coor, c);
         firstMove = true;
+        if (c == 0) {
+            this.setIcon(new ImageIcon("/Icons/blackPawn.png").getScaledInstance(Test.width/8,Test.height/8, Image.SCALE_SMOOTH));   
+        } else {
+            this.setIcon(new ImageIcon("/Icons/whitePawn.png").getScaledInstance(Test.width/8,Test.height/8, Image.SCALE_SMOOTH));
+        }
     }
     
     public ArrayList<ArrayList<Coords>> getValidMoves() {
