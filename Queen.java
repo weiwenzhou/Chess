677b7f65a863dc48b8f3c268b52834577e07b8b9
jsun5 <jsun5@stuy.edu>
2018-01-08 20:52:31 -0500

    wrote getValidMoves for Rook

--- Queen.java
+++ Queen.java
@@ -33,8 +33,4 @@ public class Queen extends Piece{
         return "";
     }
     
-    private void setIcon() {
-        
-    }
-
 }
--- Rook.java
+++ Rook.java
@@ -5,28 +5,34 @@ import java.util.ArrayList;
 public class Rook extends Piece{
     private boolean firstMove;
     
-    public Rook(int, int, int) {
-        
+    public Rook(int x, int y, int col) {
+        super (x, y, col);
+        firstMove = true;
     }
     
-    public Rook(Coords, int) {
-        
+    public Rook(Coords coor, int col) {
+        super (coor, col);
+        firstMove = true;
     }
     
     public ArrayList<ArrayList<Coords>> getValidMoves() {
-        
+        ArrayList<ArrayList<Coords>> validSet = new ArrayList<ArrayList<Coords>>();
+        int xcor = getPosition().getX();
+        int ycor = getPosition().getY();
+        //Linear
+        validSet.add(getMoves(xcor,ycor, -1, 0));
+        validSet.add(getMoves(xcor,ycor, 1, 0));
+        validSet.add(getMoves(xcor,ycor, 0, -1));
+        validSet.add(getMoves(xcor,ycor, 0, 1));
+        return validSet;
     }
     
     public void notFirst() {
-        
+        firstMove = false;
     }
     
     public String toString() {
-        
-    }
-    
-    private void setIcon() {
-        
+        return ""
     }
 
 }
