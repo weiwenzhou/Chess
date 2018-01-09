fbc38d648bf5cd9067d5cef757e272644b7588ad
jsun5 <jsun5@stuy.edu>
2018-01-08 20:26:56 -0500

    wrote getValidMoves for Queen, implementing getMoves from Piece

--- Queen.java
+++ Queen.java
@@ -4,20 +4,33 @@ import java.util.ArrayList;
 
 public class Queen extends Piece{
     
-    public Queen( x, int y, int) {
-        
+    public Queen(int x, int y, int col) {
+        super (x, y, col);
     }
     
-    public Queen(Coords, int) {
-        
+    public Queen(Coords coor, int col) {
+        super (coor, col);
     }
     
     public ArrayList<ArrayList<Coords>> getValidMoves() {
-        
+        ArrayList<ArrayList<Coords>> validSet = new ArrayList<ArrayList<Coords>>();
+        int xcor = getPosition().getX();
+        int ycor = getPosition().getY();
+        //Diagonal
+        validSet.add(getMoves(xcor,ycor, -1, -1));
+        validSet.add(getMoves(xcor,ycor, -1, 1));
+        validSet.add(getMoves(xcor,ycor, 1, -1));
+        validSet.add(getMoves(xcor,ycor, 1, 1));
+        //Linear
+        validSet.add(getMoves(xcor,ycor, -1, 0));
+        validSet.add(getMoves(xcor,ycor, 1, 0));
+        validSet.add(getMoves(xcor,ycor, 0, -1));
+        validSet.add(getMoves(xcor,ycor, 0, 1));
+        return validSet;
     }
     
     public String toString() {
-        
+        return "";
     }
     
     private void setIcon() {
