51d01f5d2c4ddd09d2e2b9a4ec85a998e2e801d4
wzhou2 <wzhou2@stuy.edu>
2018-01-14 16:32:37 -0500

    Made the topBar with letterBar that has letter A-H

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 public class Main extends JFrame {
     
     private Container pane;
-    private JPanel letterBar, numberBar, playerBar, topBar, board;
+    private JPanel numberBar, playerBar, topBar, board;
     public static int barWidth = 32;
     public static int timerWidth = 256;
     public static Font fontStyle = new Font("Comic Sans Ms", Font.BOLD, barWidth);
@@ -22,13 +22,52 @@ public class Main extends JFrame {
         
         // JPanel initialize
         board = new Board();
+        topBar = makeTopBar();
         
         // Adding sections
         pane.add(board, BorderLayout.CENTER);
+        pane.add(topBar, BorderLayout.PAGE_START);
         
         pack();
     }
     
+    private JPanel makeLetterBar() {
+        JPanel letterBar = new JPanel(new GridLayout(1,8));
+        for (int x = 0; x < 8; x++) {
+            JLabel token;
+            
+            char letter = (char) (65+x);
+            token = new JLabel(""+letter, JLabel.CENTER);
+            token.setPreferredSize(new Dimension(Board.width/8,barWidth));
+            token.setFont(fontStyle);
+            
+            token.setBorder(Board.standard);
+            letterBar.add(token);
+        }
+        return letterBar;
+    }
+
+    private JPanel makeTopBar() {
+        JPanel top = new JPanel(new BorderLayout());
+        
+        // letterBar
+        JPanel letterBar = makeLetterBar();
+        
+        // Left Side Box
+        JLabel leftBox = new JLabel();
+        leftBox.setPreferredSize(new Dimension(barWidth,barWidth));
+        
+        // Right Side Box
+        JLabel rightBox = new JLabel();
+        rightBox.setPreferredSize(new Dimension(timerWidth,barWidth));
+        
+        
+        top.add(leftBox, BorderLayout.LINE_START);
+        top.add(rightBox, BorderLayout.LINE_END);
+        top.add(letterBar, BorderLayout.CENTER);
+        
+        return top;
+    }
     
     public static void main(String[] args) {
         Main test = new Main();
